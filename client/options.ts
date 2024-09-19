51c89c1280052dcd0e94b8acbe07ca2d5bf983f8
WRXinYue <3322543587@qq.com>
2024-09-20 02:59:00 +0800

    refactor: change window.location to vueuse

--- client/options.ts
+++ client/options.ts
@@ -1,16 +1,19 @@
 import { computed } from 'vue'
 import type { ValaxyAddon } from 'valaxy'
 import { useRuntimeConfig } from 'valaxy'
+import { useBrowserLocation } from '@vueuse/core'
 import type { VercountOptions } from '../types'
 
 export function useAddonVercountConfig() {
   const runtimeConfig = useRuntimeConfig()
+  const location = useBrowserLocation()
+
   return computed<VercountOptions>(() => {
     const options = (runtimeConfig.value.addons['valaxy-addon-vercount'] as ValaxyAddon<VercountOptions>).options
 
     return {
       placeholder: '-',
-      baseUrl: window.location.origin,
+      baseUrl: location.value.origin,
       ...options,
     }
   })
